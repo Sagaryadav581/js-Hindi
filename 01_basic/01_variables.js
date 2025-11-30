40bf0d4b1dd69d41868e99068064f12b27bc430e
Sagaryadav581 <sagaryadav9562490@gmail.com>
2025-11-30 16:06:01 +0530

    changes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 29992
+let accountEmail = "sagar@google.com"
+var accountPassword = "38218"
+accountCity = "jaipur"
+
+accountEmail= "sagr@google.com"
+accountPassword= "525255"
+accountCity="mumbai"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,29 @@
+"use strick";//treat all js code as new vieson
+
+//alert(3+3) we are using node js , not browser//
+
+console.log(3+
+    3) // code readability should be high//
+
+console.log("sagar");
+   
+
+let name = "sagar"
+let age = 18
+ let isLoggedIn = false
+
+
+//  number => 2 to power 53
+// bigint
+// string =>"sagar"
+// boolean => true/false
+// null => standaloon value
+// undifine=>
+// symbol=> unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
